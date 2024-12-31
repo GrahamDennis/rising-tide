55e50d2adcc409935ea5df69067f2513afa9394b
Graham Dennis <graham.dennis@gmail.com>
2024-12-31 11:58:33 +1100

    Add a placeholder default package

--- flake.nix
+++ flake.nix
@@ -3,14 +3,30 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "https://flakehub.com/f/hercules-ci/flake-parts/*";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
   };
 
-  outputs = inputs @ { self, nixpkgs, ... }: let
+  outputs = inputs @ { self, nixpkgs, flake-parts, ... }: let
     root = ./.;
     lib = nixpkgs.lib;
     risingTideLib = import (root + "/lib.nix") {inherit lib;};
-    inherit (risingTideLib.mkInjector { inherit root lib risingTideLib; }) inject injectModule;
-  in {
-    lib = risingTideLib;
-  };
+    inherit (risingTideLib.mkInjector { inherit root lib risingTideLib inputs; }) inject injectModule;
+  in     flake-parts.lib.mkFlake {
+      inherit inputs;
+      specialArgs = {
+        inherit root risingTideLib inject injectModule;
+      };
+  } ({self, ...}: let in {
+    systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+    perSystem = { pkgs, ...}: {
+      # FIXME: temporary
+      packages.default = pkgs.emptyFile;
+    };
+    flake = {
+      inherit self;
+      lib = risingTideLib;
+    };
+  })
+;
 }
