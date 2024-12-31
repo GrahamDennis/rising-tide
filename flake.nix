d1d698e967439f7dc70ac7ab65c0033e60160b7f
Graham Dennis <graham.dennis@gmail.com>
2024-12-31 11:28:38 +1100

    Add empty lib

--- flake.nix
+++ flake.nix
@@ -5,8 +5,12 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = inputs @ { self, nixpkgs, ... }: {
-    
+  outputs = inputs @ { self, nixpkgs, ... }: let
+    root = ./.;
+    lib = nixpkgs.lib;
+    risingTideLib = import (root + "/lib.nix") {inherit lib;};
 
+  in {
+    lib = risingTideLib;
   };
 }
