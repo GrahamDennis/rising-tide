0fe50d5841ad7c80206108703a1642d50a1ac144
Graham Dennis <graham.dennis@gmail.com>
2024-12-31 11:27:05 +1100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "Standardardised nix utilities";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = inputs @ { self, nixpkgs, ... }: {
+    
+
+  };
+}
