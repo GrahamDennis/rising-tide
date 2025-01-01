9c8ec32f722a6923a231e28c5539b4ab1447ae14
Graham Dennis <graham.dennis@gmail.com>
2025-01-01 16:11:07 +1100

    don't modify nix and nix-unit

--- devShell.nix
+++ devShell.nix
@@ -1,13 +1,8 @@
 # rising-tide flake context
 {inputs, ...}:
 # rising-tide perSystem context 
-{pkgs, ...}: let
-  overlay = final: prev: {
-    nixVersions = import "${inputs.nixpkgs}/lib/tests/nix-for-tests.nix" { pkgs = prev; };
-  };
-  pkgsWithOverlay = pkgs.extend overlay;
-in
+{pkgs, ...}: 
 pkgs.mkShell {
     name = "rising-tide-root";
-    nativeBuildInputs = with pkgsWithOverlay; [ nix-unit go-task ];
+    nativeBuildInputs = with pkgs; [ nix-unit go-task ];
 }
