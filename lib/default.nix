c297c152646f77a644c9602f23cece71c0316820
Graham Dennis <graham.dennis@gmail.com>
2025-01-05 08:06:53 +1100

    Rename lib/bootstrap.nix to lib/injector.nix

--- lib/default.nix
+++ lib/default.nix
@@ -4,32 +4,24 @@
   ...
 }:
 let
-  risingTideBootstrapLib = import ./bootstrap.nix { inherit lib; };
-  risingTideLib = lib.makeExtensible (
-    self:
-    let
-      injector = risingTideBootstrapLib.mkInjector "injector" {
-        args = {
-          inherit lib risingTide;
-          risingTideLib = self;
-        };
-      };
-    in
-    {
-      inherit injector;
+  risingTideInjectorLib = import ./injector.nix { inherit lib; };
+  injector = risingTideInjectorLib.mkInjector "injector" {
+    args = {
+      inherit lib risingTide risingTideLib;
+    };
+  };
+  risingTideLib = {
+    attrs = injector.inject ./attrs.nix;
+    injector = injector.inject ./injector.nix;
+    project = injector.inject ./project.nix;
+    strings = injector.inject ./strings.nix;
+    testutils = injector.inject ./testutils.nix;
+    types = injector.inject ./types.nix;
 
-      attrs = injector.inject ./attrs.nix;
-      bootstrap = injector.inject ./bootstrap.nix;
-      project = injector.inject ./project.nix;
-      strings = injector.inject ./strings.nix;
-      testutils = injector.inject ./testutils.nix;
-      types = injector.inject ./types.nix;
-
-      inherit (self.attrs) filterAttrsByPathRecursive;
-      inherit (self.bootstrap) callWithLazyArgs mkInjector getLazyArgFromConfig;
-      inherit (self.strings) sanitizeBashIdentifier;
-      inherit (self.project) mkBaseProject mkProject;
-    }
-  );
+    inherit (risingTideLib.attrs) filterAttrsByPathRecursive;
+    inherit (risingTideLib.injector) callWithLazyArgs mkInjector getLazyArgFromConfig;
+    inherit (risingTideLib.strings) sanitizeBashIdentifier;
+    inherit (risingTideLib.project) mkBaseProject mkProject;
+  };
 in
 risingTideLib
