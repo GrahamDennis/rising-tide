4db26b6d735e119519f3f4f981a6e45eab0d9bb7
Graham Dennis <graham.dennis@gmail.com>
2025-01-21 21:14:13 +1100

    Update module options documentation

--- packages/project-module-docs.nix
+++ packages/project-module-docs.nix
@@ -30,9 +30,26 @@ let
       fixupsModule
     ];
   };
+
+  # Logic copied from nixpkgs/doc/doc-support/options-doc.nix
+  root = toString self;
+
+  transformDeclaration =
+    decl:
+    let
+      declStr = toString decl;
+      subpath = lib.removePrefix "/" (lib.removePrefix root declStr);
+    in
+    assert lib.hasPrefix root declStr;
+    {
+      url = "https://github.com/GrahamDennis/rising-tide/blob/main/${subpath}";
+      name = subpath;
+    };
+
 in
 (pkgs.nixosOptionsDoc {
   inherit (evaluatedProjectModule) options;
   documentType = "none";
+  transformOptions = opt: opt // { declarations = map transformDeclaration opt.declarations; };
   warningsAreErrors = false;
 }).optionsCommonMark
