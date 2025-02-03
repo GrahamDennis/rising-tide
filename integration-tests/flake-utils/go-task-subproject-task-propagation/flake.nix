867dbdb6e33a68bb4e285e8244a387ef99b12a6b
Graham Dennis <graham.dennis@gmail.com>
2025-02-04 07:37:09 +1100

    fix the subtask propagation integration test

--- integration-tests/flake-utils/go-task-subproject-task-propagation/flake.nix
+++ integration-tests/flake-utils/go-task-subproject-task-propagation/flake.nix
@@ -28,6 +28,7 @@
           subprojects.subproject = {
             relativePaths.toParentProject = "subproject";
             tools.go-task = {
+              enable = true;
               taskfile.tasks.hello.cmds = [ "echo 'Hello, World!'" ];
             };
           };
