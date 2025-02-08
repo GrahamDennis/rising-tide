b545ccf33da6de97b2a593cdf1cbd627b98e7fbe
Graham Dennis <graham.dennis@gmail.com>
2025-02-08 13:34:47 +1100

    Update the documentation to include new lib.* function groups

--- packages/lib-docs.nix
+++ packages/lib-docs.nix
@@ -14,6 +14,8 @@ let
     risingTideLib = {
       attrs = ../lib/attrs.nix;
       injector = ../lib/injector.nix;
+      nixagoEngines = ../lib/nixagoEngines.nix;
+      overlays = ../lib/overlays.nix;
       project = ../lib/project.nix;
       strings = ../lib/strings.nix;
       tests = ../lib/tests.nix;
@@ -31,6 +33,8 @@ pkgs.runCommand "lib-docs"
 
     nixdoc --category "" --description "" --prefix "risingTideLib.attrs" --file ${sources.risingTideLib.attrs} > $out/lib.attrs.md
     nixdoc --category "" --description "" --prefix "risingTideLib.injector" --file ${sources.risingTideLib.injector} > $out/lib.injector.md
+    nixdoc --category "" --description "" --prefix "risingTideLib.nixagoEngines" --file ${sources.risingTideLib.nixagoEngines} > $out/lib.nixagoEngines.md
+    nixdoc --category "" --description "" --prefix "risingTideLib.overlays" --file ${sources.risingTideLib.overlays} > $out/lib.overlays.md
     nixdoc --category "" --description "" --prefix "risingTideLib.project" --file ${sources.risingTideLib.project} > $out/lib.project.md
     nixdoc --category "" --description "" --prefix "risingTideLib.strings" --file ${sources.risingTideLib.strings} > $out/lib.strings.md
     nixdoc --category "" --description "" --prefix "risingTideLib.tests" --file ${sources.risingTideLib.tests} > $out/lib.tests.md
