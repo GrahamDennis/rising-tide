e4d4d91238273c16e17f79b6369458200ccc352d
Graham Dennis <graham.dennis@gmail.com>
2025-02-23 13:00:38 +1100

    Add documentation for the minimal subflake

--- minimal/flake.nix
+++ minimal/flake.nix
@@ -11,6 +11,11 @@
       );
     in
     {
+      # The flake output attributes must be explicitly listed so that nix doesn't get into an infinite
+      # recursion problem. If the flake outputs produced the attribute `self`, then that would override
+      # the default `self` attribute and would make the import above not work. By explicitly listing the
+      # attributes here, nix can identify that the `self` attribute must resolve to the usual one and therefore
+      # can use it in the import above.
       inherit (flakeOutputs)
         devShells
         debug
