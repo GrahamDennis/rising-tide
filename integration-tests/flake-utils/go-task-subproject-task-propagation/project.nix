665e7275bca12822dedf253051284a0ed5580fc0
Graham Dennis <graham.dennis@gmail.com>
2025-03-07 19:55:54 +1100

    Enable mkShell in the go-task subproject

--- integration-tests/flake-utils/go-task-subproject-task-propagation/project.nix
+++ integration-tests/flake-utils/go-task-subproject-task-propagation/project.nix
@@ -3,6 +3,7 @@
   relativePaths.toRoot = "./.";
   subprojects.subproject = {
     relativePaths.toParentProject = "subproject";
+    mkShell.enable = true;
     tools.go-task = {
       enable = true;
       taskfile.tasks.hello.cmds = [ "echo 'Hello, World!'" ];
