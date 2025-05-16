GrahamDennis/rising-tide

commit 124986edd5d36b5718bfc71da697946b9353cc11

fix: Ensure non-formatting linters run before formatting linters (#58)
If we ran formatting linters first, they might want to reformat the code
again
after non-formatting linters (e.g. dead code detectors) run.

---------

Co-authored-by: Graham Dennis <gdennis@anduril.com>

modules/projects/tools/ruff.nix
@@ -59,6 +59,8 @@ in
                 "*.py"
                 "*.pyi"
               ];
+              # Ensure linters that don't format run first
+              priority = -10;
             };
             formatter.ruff-format = lib.mkIf cfg.format.enable {
               command = ruffExe;

modules/projects/tools/clang-tidy.nix
@@ -59,6 +59,8 @@ in
               "*.hh"
               "*.hpp"
             ];
+            # Ensure linters that don't format run first
+            priority = -10;
           };
         };
       };

modules/projects/tools/clangd-tidy.nix
@@ -52,6 +52,8 @@ in
               "*.hh"
               "*.hpp"
             ];
+            # Ensure linters that don't format run first
+            priority = -10;
           };
         };
       };

modules/projects/tools/deadnix.nix
@@ -34,6 +34,8 @@ in
             includes = [
               "*.nix"
             ];
+            # Ensure linters that don't format run first
+            priority = -10;
           };
         };
       };

modules/projects/tools/keep-sorted.nix
@@ -26,6 +26,8 @@ in
           formatter.keep-sorted = {
             command = keepSortedExe;
             includes = [ "*" ];
+            # Ensure linters that don't format run first
+            priority = -10;
           };
         };
       };

modules/projects/tools/shellcheck.nix
@@ -48,6 +48,8 @@ in
               "*.bash"
               "*.bats"
             ];
+            # Ensure linters that don't format run first
+            priority = -10;
           };
         };
       };

modules/projects/tools/statix.nix
@@ -50,6 +50,8 @@ in
             includes = [
               "*.nix"
             ];
+            # Ensure linters that don't format run first
+            priority = -10;
           };
         };
       };

commit 12d1b554ba453aee8b6e41441abaca004a67be4e

Fix python formatting (#67)
Fix a regression in https://github.com/GrahamDennis/rising-tide/pull/58

Before https://github.com/GrahamDennis/rising-tide/pull/58, `ruff
format` would run before `ruff check --fix` which would ensure that any
long lines that could be rewrapped would automatically be done before
`ruff check --fix` ran. Unfortunately when `ruff check --fix` ran, it
would modify the code potentially in such a way that `ruff format` would
want to reformat the code again. In that PR, the order was swapped so
that now `ruff check --fix` runs first. This creates a new problem where
long lines will cause `ruff check --fix` to complain, but these are
later fixed by `ruff format`.

The fix proposed here is basically to split treefmt execution into two
phases:
1. Code modification without failure
2. Code validation with failure

For python this means that `ruff check --fix-only` runs first, then
`ruff format` and finally `ruff check`.

modules/projects/tools/ruff.nix
@@ -49,11 +49,11 @@ in
         treefmt = {
           enable = true;
           config = {
-            formatter.ruff-lint = lib.mkIf cfg.lint.enable {
+            formatter.ruff-lint-fix-only = lib.mkIf cfg.lint.enable {
               command = ruffExe;
               options = [
                 "check"
-                "--fix"
+                "--fix-only"
               ];
               includes = [
                 "*.py"
@@ -61,6 +61,18 @@ in
               ];
               # Ensure linters that don't format run first
               priority = -10;
+            };
+            formatter.ruff-lint = lib.mkIf cfg.lint.enable {
+              command = ruffExe;
+              options = [
+                "check"
+              ];
+              includes = [
+                "*.py"
+                "*.pyi"
+              ];
+              # Ensure read-only linters run last
+              priority = 10;
             };
             formatter.ruff-format = lib.mkIf cfg.format.enable {
               command = ruffExe;

integration-tests/flake-utils/python-package/test.bats
@@ -116,3 +116,15 @@ teardown() {
   run task ci
   assert_success
 }
+
+@test "check:treefmt task successfully reformats long lines" {
+  restore_src_in_teardown
+  cat <<EOF >>src/python_package/__init__.py
+if True and True and True and True and True and True and True and True and True and True and True and True and True and True and True and True and True and True:
+    pass
+
+EOF
+
+  run task check:treefmt
+  assert_success
+}