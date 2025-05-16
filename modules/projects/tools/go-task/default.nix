18b2e68835975aa492f865afd1b0b3b88b77a799
Graham Dennis <graham.dennis@gmail.com>
2025-05-17 09:47:04 +1000

    Colourise the heading and footer for each task's output (#69)

--- modules/projects/tools/go-task/default.nix
+++ modules/projects/tools/go-task/default.nix
@@ -67,44 +67,67 @@ in
         } ./go-task-setup-hook.sh)
       ];
       tools = {
-        go-task.taskfile = {
-          # The default output format of interleaved does not do line-buffering. As a result,
-          # interleaved terminal codes (e.g. colours) can get mixed up with the output of other
-          # terminal codes confusing the terminal.
-          output = lib.mkDefault {
-            group = {
-              begin = "::group::{{.ALIAS}}";
-              end = "::endgroup::";
+        go-task.taskfile =
+          let
+            colourEscape = "";
+          in
+          {
+            # The default output format of interleaved does not do line-buffering. As a result,
+            # interleaved terminal codes (e.g. colours) can get mixed up with the output of other
+            # terminal codes confusing the terminal.
+            vars = {
+              _GROUP_COLOURS = builtins.concatStringsSep "," [
+                "${colourEscape}[1;33m" # bold yellow
+                "${colourEscape}[1;34m" # bold blue
+                "${colourEscape}[1;35m" # bold magenta
+                "${colourEscape}[1;32m" # bold green
+                "${colourEscape}[1;36m" # bold cyan
+                "${colourEscape}[1;93m" # bold high-intensity yellow
+                "${colourEscape}[1;94m" # bold high-intensity blue
+                "${colourEscape}[1;95m" # bold high-intensity magenta
+                "${colourEscape}[1;92m" # bold high-intensity green
+                "${colourEscape}[1;96m" # bold high-intensity cyan
+              ];
             };
-          };
-          includes = lib.mkMerge (
-            lib.mapAttrsToList (name: subprojectConfig: {
-              ${name} = {
-                taskfile = subprojectConfig.relativePaths.fromParentProject;
-                dir = subprojectConfig.relativePaths.fromParentProject;
-              };
-            }) enabledSubprojects
-          );
-          tasks = lib.mkMerge (
-            (lib.mapAttrsToList (
-              _name: subprojectConfig:
-              lib.genAttrs (getCfg subprojectConfig).inheritedTasks (taskName: {
-                deps = [ "${subprojectConfig.name}:${taskName}" ];
-              })
-            ) enabledSubprojects)
-            ++ [
-              {
-                "nix-build:*" = {
-                  desc = "Build a package with `nix build`";
-                  vars.PACKAGE = "{{index .MATCH 0}}";
-                  label = "nix-build:{{.PACKAGE}}";
-                  prefix = "nix-build:{{.PACKAGE}}";
-                  cmds = [ "nix build --show-trace --log-lines 500 .?submodules=1#{{.PACKAGE}}" ];
+            output = lib.mkDefault {
+              group =
+                let
+                  setColour = ''{{$colours := splitList "," ._GROUP_COLOURS }}{{ index $colours (mod (adler32sum .ALIAS) (len $colours)) }}'';
+                  resetColour = "${colourEscape}[0m";
+                in
+                {
+                  begin = "${setColour}[BEGIN] {{.ALIAS}}${resetColour}";
+                  end = "${setColour}[END]   {{.ALIAS}}${resetColour}";
                 };
-              }
-            ]
-          );
-        };
+            };
+            includes = lib.mkMerge (
+              lib.mapAttrsToList (name: subprojectConfig: {
+                ${name} = {
+                  taskfile = subprojectConfig.relativePaths.fromParentProject;
+                  dir = subprojectConfig.relativePaths.fromParentProject;
+                };
+              }) enabledSubprojects
+            );
+            tasks = lib.mkMerge (
+              (lib.mapAttrsToList (
+                _name: subprojectConfig:
+                lib.genAttrs (getCfg subprojectConfig).inheritedTasks (taskName: {
+                  deps = [ "${subprojectConfig.name}:${taskName}" ];
+                })
+              ) enabledSubprojects)
+              ++ [
+                {
+                  "nix-build:*" = {
+                    desc = "Build a package with `nix build`";
+                    vars.PACKAGE = "{{index .MATCH 0}}";
+                    label = "nix-build:{{.PACKAGE}}";
+                    prefix = "nix-build:{{.PACKAGE}}";
+                    cmds = [ "nix build --show-trace --log-lines 500 .?submodules=1#{{.PACKAGE}}" ];
+                  };
+                }
+              ]
+            );
+          };
         nixago.requests = lib.mkIf (cfg.taskfile != { }) [
           {
             data = cfg.configFile;
